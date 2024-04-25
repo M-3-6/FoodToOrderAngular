8bfdeaa7aa5ed746389d1e2f1e92d20f06c87489
M-3-6 <mariavijigeorge@gmail.com>
2024-04-25 18:28:19 +0530

    ui changes

--- src/app/components/order/view-order/view-order.component.ts
+++ src/app/components/order/view-order/view-order.component.ts
@@ -6,6 +6,7 @@ import { User } from '../../../models/user';
 import { Order } from '../../../models/order';
 import { OrderService } from '../../../services/order.service';
 import { Address } from '../../../models/address';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-view-order',
@@ -15,14 +16,15 @@ import { Address } from '../../../models/address';
 export class ViewOrderComponent {
   currencyCode: string = 'INR'
   arrUsers: Observable<User[]>;
-
+  flag: number = 0;
   arrOrders: Observable<Order[]> = new Observable<Order[]>();
 
   addId: number = 1;
   addDishId: number = 1;
   constructor(
     private userService: UserService,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private messageService:MessageService
   ) {
     this.arrUsers = this.userService.getUsers();
   }
@@ -30,6 +32,20 @@ export class ViewOrderComponent {
   onUserSelected(evt: any) {
     console.log('user selected: ', evt.target.value);
     this.arrOrders = this.orderService.getOrdersByUserId(evt.target.value);
+  }
+
+  resetFlag() {
+    this.flag = 0;
+  }
+
+  onNoOrdersPrompted() {
+    this.flag = 1;
+    this.messageService.add({
+      key: 'tr',
+      severity: 'warn',
+      summary: 'Warning',
+      detail: 'No Orders!',     
+  });
   }
 
 }
