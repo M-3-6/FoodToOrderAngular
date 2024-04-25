2735427b05f0af16c7edeba7d6c641fad2da8b6a
Prajith Shetty <prajithshettyj@gmail.com>
2024-04-25 14:40:03 +0530

    impure pipe added

--- src/app/pipes/open-rest-filter.pipe.ts
+++ src/app/pipes/open-rest-filter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Restaurant } from '../models/restaurant';
 
 @Pipe({
-  name: 'openRestFilter'
+  name: 'openRestFilter',
+  pure: false
 })
 export class OpenRestFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(restaurants:Restaurant[] ): Restaurant[] {
+    if(restaurants==null){
+      return restaurants;
+    }
+    return restaurants.filter(rest=>rest.isOpen==true)
   }
 
 }
