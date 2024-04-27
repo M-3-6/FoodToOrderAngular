9637717edc67c332f8a945e4ad3f2a61c62bdb4a
M-3-6 <mariavijigeorge@gmail.com>
2024-04-27 16:27:36 +0530

    fixed cart issues

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -10,92 +10,91 @@ import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent implements OnInit {
- cart:Cart = new Cart(0,0,[],[])
- cartId:number=0
- order:Order=new Order(0,'',0,0,[],[]);
+  cart: Cart = new Cart(0, 0, [], []);
+  cartId: number = 0;
+  order: Order = new Order(0, '', 0, 0, [], []);
 
-  constructor(private cartService:CartService,private orderService:OrderService){
-    console.log("cart constructor called")
-    
+  constructor(
+    private cartService: CartService,
+    private orderService: OrderService
+  ) {
+    console.log('cart constructor called');
   }
 
   ngOnInit(): void {
-    console.log("cart ngOnInit called")
-    console.log(localStorage.getItem('userId'))
-    if(localStorage.getItem('userId')!=null){
-      var cartId = localStorage.getItem('userId')?localStorage.getItem('userId'):"0"
-      this.cartId = parseInt(cartId?cartId:"0");
-      
-      this.cartService.getCartById(this.cartId.toString()).subscribe(data=>{
-        this.cart = data;
-        console.log("cart details:", this.cart)
-       })
+    console.log('cart ngOnInit called');
+    console.log(localStorage.getItem('userId'));
+    if (localStorage.getItem('userId') != null) {
+      var cartId = localStorage.getItem('userId')
+        ? localStorage.getItem('userId')
+        : '0';
+      this.cartId = parseInt(cartId ? cartId : '0');
 
+      this.cartService.getCartById(this.cartId.toString()).subscribe((data) => {
+        this.cart = data;
+        console.log('cart details:', this.cart);
+      });
     }
   }
 
-
-
-
- IncreaseQty(index:number){
+  IncreaseQty(index: number) {
     this.cart.quantity[index]++;
     this.cart.Amount += this.cart.arrDishes[index].price;
   }
 
- DecreaseQty(index:number){
+  DecreaseQty(index: number) {
     this.cart.quantity[index]--;
     this.cart.Amount -= this.cart.arrDishes[index].price;
 
-    if(this.cart.quantity[index]==0){
-      this.cart.quantity.splice(index,1)
-      this.cart.arrDishes.splice(index,1)
+    if (this.cart.quantity[index] == 0) {
+      this.cart.quantity.splice(index, 1);
+      this.cart.arrDishes.splice(index, 1);
+      if (this.cart.quantity.length == 0) {
+        this.onSaveCart();
+      }
     }
   }
 
-  Checkout(){
-    
-
-    this.orderService.getOrders().subscribe(data=>{
-      const largestId = Math.max(...data.map(item=>item.id))
-      console.log(largestId)
+  Checkout() {
+    this.orderService.getOrders().subscribe((data) => {
+      const largestId = Math.max(...data.map((item) => item.id));
+      console.log(largestId);
       this.order.id = largestId + 1;
 
-      this.order.orderDate = new Date().toLocaleDateString()
+      this.order.orderDate = new Date().toLocaleDateString();
       this.order.userId = this.cart.id;
       this.order.orderAmount = this.cart.Amount;
-      this.cart.arrDishes.forEach(dish => {
+      this.cart.arrDishes.forEach((dish) => {
         this.order.arrDishes.push(dish);
       });
 
-      this.cart.quantity.forEach(qty=>{
+      this.cart.quantity.forEach((qty) => {
         this.order.quantity.push(qty);
-      })
+      });
 
+      this.orderService.addOrder(this.order).subscribe((data) => {
+        console.log(data);
+      });
 
-      this.orderService.addOrder(this.order).subscribe(data=>{
-        console.log(data)
-      })
-      
       this.onResetCart();
+    });
   }
-)}
 
-onResetCart() {
-  this.cart.quantity = []
-  this.cart.arrDishes = []
-  this.cart.Amount = 0;
-  this.cartService.updateCart(this.cart);
-}
+  onResetCart() {
+    this.cart.quantity = [];
+    this.cart.arrDishes = [];
+    this.cart.Amount = 0;
+    this.cartService.updateCart(this.cart).subscribe(() => {
+      console.log('new cart: ', this.cart);
+    });
+  }
 
-onSaveCart() { 
-  this.cartService.updateCart(this.cart).subscribe(
-    () => {
-      console.log("current cart: ", this.cart);
-    }
-  );
-}
-
+  onSaveCart() {
+    this.cartService.updateCart(this.cart).subscribe(() => {
+      console.log('current cart: ', this.cart);
+    });
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -9,71 +9,55 @@ import { Dish } from '../models/dish';
 import { Cart } from '../models/cart';
 import { switchMap } from 'rxjs/operators';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  baseUrl: string = 'http://localhost:3000';
 
-  baseUrl:string="http://localhost:3000"
-
-  httpHeader={
-    headers:new HttpHeaders({
-      'Content-Type':'application/json'
-    })
-  }
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
 
   cartUpdated: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private httpClient:HttpClient) {
+  constructor(private httpClient: HttpClient) {}
 
-  }
-
-  httpError(error:HttpErrorResponse){ 
-    let msg=""
-    if(error.error instanceof ErrorEvent){
-      msg=error.error.message
+  httpError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      msg = error.error.message;
+    } else {
+      msg = 'Error code:${error.status}\nMessage:${error.message}';
     }
-    else{
-      msg='Error code:${error.status}\nMessage:${error.message}';
-    }
-    return throwError(msg)
+    return throwError(msg);
   }
 
   getCartById(cId: string): Observable<Cart> {
-    return this.httpClient.get<Cart>(`${this.baseUrl}/carts/${cId}`).pipe(
-      catchError(this.httpError)
-    );
+    return this.httpClient
+      .get<Cart>(`${this.baseUrl}/carts/${cId}`)
+      .pipe(catchError(this.httpError));
   }
-  
- 
-getCartByUserId(userId: string): Observable<Cart> {
-  return this.httpClient.get<Cart>(`${this.baseUrl}/carts?userId=${userId}`).pipe(
-    catchError(this.httpError)
-  );
-}
 
-updateCart(cart: Cart): Observable<Cart> {
-  this.cartUpdated.emit();
-  return this.httpClient.put<Cart>(
-    `${this.baseUrl}/carts/${cart.id}`,
-    cart,
-    this.httpHeader
-  ).pipe(
-    catchError(this.httpError)
-  );
-}
+  getCartByUserId(userId: string): Observable<Cart> {
+    return this.httpClient
+      .get<Cart>(`${this.baseUrl}/carts?userId=${userId}`)
+      .pipe(catchError(this.httpError));
+  }
 
-addCart(cart: Cart): Observable<Cart> {
-  return this.httpClient.post<Cart>(
-    `${this.baseUrl}/carts/`,
-    cart,
-    this.httpHeader
-  ).pipe(
-    catchError(this.httpError)
-  );
-}
+  updateCart(cart: Cart): Observable<Cart> {
+    console.log('updated cart:', cart);
+    this.cartUpdated.emit();
+    return this.httpClient
+      .put<Cart>(`${this.baseUrl}/carts/${cart.id}`, cart, this.httpHeader)
+      .pipe(catchError(this.httpError));
+  }
 
-
-  
+  addCart(cart: Cart): Observable<Cart> {
+    return this.httpClient
+      .post<Cart>(`${this.baseUrl}/carts/`, cart, this.httpHeader)
+      .pipe(catchError(this.httpError));
+  }
 }
