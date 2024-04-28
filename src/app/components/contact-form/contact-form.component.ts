dad07934f62eb581ebdd6d17382b40a41886ec59
M-3-6 <mariavijigeorge@gmail.com>
2024-04-28 13:18:53 +0530

    added contact form submit message

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -4,46 +4,37 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
 export class ContactFormComponent {
   contactForm: FormGroup;
-  
-    constructor(private fb: FormBuilder) {
-      this.contactForm = this.fb.group({
-        name: [
-          '',
-          [
-            Validators.required,
-          ],
-        ],
-        email: ['', [Validators.required, Validators.email]],
-        phone: [''],
-        message: [
-          '',
-          [
-            Validators.required,
-            Validators.maxLength(500),
-          ],
-        ],
-      });
+  validMsg: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      message: ['', [Validators.required, Validators.maxLength(500)]],
+    });
+  }
+
+  submitForm() {
+    if (this.contactForm.valid) {
+      this.validMsg = true;
+      console.log('Form submitted with data:', this.contactForm.value);
+    } else {
+      this.markFormGroupTouched(this.contactForm);
     }
-  
-    submitForm() {
-      if (this.contactForm.valid) {
-        console.log('Form submitted with data:', this.contactForm.value);
-      } else {
-        this.markFormGroupTouched(this.contactForm);
+  }
+
+  markFormGroupTouched(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
       }
-    }
-  
-    markFormGroupTouched(formGroup: FormGroup) {
-      Object.values(formGroup.controls).forEach((control) => {
-        control.markAsTouched();
-  
-        if (control instanceof FormGroup) {
-          this.markFormGroupTouched(control);
-        }
-      });
-    }
+    });
+  }
 }
